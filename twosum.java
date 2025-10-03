ae20fe51301a5d05345d4b287d8d8452974a57a4
akshdeepchaddha <akshdeepsinghchaddha@gmail.com>
2025-10-03 17:31:21 +0530

    Simple Sum of 2 Numbers

---
+++ twosum.java
@@ -0,0 +1,9 @@
+public class twosum {
+    public static void main(){
+        int a = 10;
+        int b = 2;
+        int sum = a+b;
+        System.out.println("Sum of 10 and 2 ="+ sum );
+    }
+}
+
